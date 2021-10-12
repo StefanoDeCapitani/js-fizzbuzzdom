75cd0765f9b9845251eab4aba8abd8f183c0bf83
Stefano De Capitani <stefano.deca@live.com>
2021-10-12 15:28:57 +0200

    Fizz, Buzz or FizzBuzz instead of numbers

--- js/main.js
+++ js/main.js
@@ -6,24 +6,22 @@ for (let i = 1; i <= 100; i++) {
   let cell = document.createElement("div");
   cell.classList.add("col");
   let cellRatio = document.createElement("div");
-  cellRatio.classList.add("ratio", "ratio-1x1", "cell");
+  cellRatio.classList.add("ratio", "cell");
+  let cellContent = document.createElement("div");
+  cellContent.textContent = i;
   if (i % 3 === 0 && i % 5 === 0) {
     cellRatio.classList.add("fizzbuzz");
+    cellContent.textContent = "FizzBuzz";
   } else {
     if (i % 3 === 0) {
       cellRatio.classList.add("fizz");
+      cellContent.textContent = "Fizz";
     }
     if (i % 5 === 0) {
       cellRatio.classList.add("buzz");
+      cellContent.textContent = "Buzz";
     }
   }
-  let cellContent = document.createElement("div");
-  cellContent.classList.add(
-    "d-flex",
-    "justify-content-center",
-    "align-items-center"
-  );
-  cellContent.textContent = i;
   cellRatio.append(cellContent);
   cell.append(cellRatio);
   row.append(cell);
